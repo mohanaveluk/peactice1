1b7f2f1059900894e677cf3dd3f787d465748037
Mohanavelu <mohanaveluk@gmail.com>
2021-08-15 10:22:00 -0500

    updated new concepts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,9 +12,61 @@ export class AppComponent {
   title = 'My page title - 2';
 
 
+  arrayObject = [34, 56, 65, 22, 99];
+  userids: any = [];
+  selectedArray: number = 0;
+  selectedItem: string = '';
+
+  editSelectedIndex: number = -1;
+  editSelectedItem: string = '';
+  editSelectedItemTemp: string = '';
+
+  userAction: number = 0;
+
   displayname(){
     this.userId = "90";
     console.log(this.userId);
   }
+
+  // Add = 1, Edit = 2, Delete = 3
+
+  setArrayValue(){
+    this.selectedItem = '';
+    this.editSelectedItem = '';
+    //this.selectedArray = this.arrayObject[2];
+
+    if(this.editSelectedIndex >= 0){
+      this.userids[this.editSelectedIndex] = this.userId;
+      this.selectedItem = this.editSelectedItemTemp;
+    }
+    else{
+      this.userids.push(this.userId);
+    }
+    this.selectedArray = this.userids.length;
+    this.userId = '';
+    this.editSelectedIndex = -1;
+  }
+
+
+
+  editItem(item: number){
+    this.selectedItem = '';
+    this.userAction = 2;
+    this.editSelectedItemTemp = this.userids[item];
+    this.editSelectedIndex = item;
+    this.userId = this.userids[item];
+
+  }
+
+  removeItem(item: number){
+    this.selectedItem = '';
+    this.userAction = 3;
+
+    console.log(item);
+    this.selectedItem = this.userids[item];
+    this.userids.splice(item, 1);
+
+  }
+
 
 }
