2731d62a09f2e51f6863a6d959506cfe949e9ca3
Mohanavelu <mohanaveluk@gmail.com>
2021-08-19 13:19:47 -0500

    implementation of API consumption

--- src/app/Section1/product/product.component.ts
+++ src/app/Section1/product/product.component.ts
@@ -1,4 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ProductListResponse } from 'src/app/services/product/product.model';
 import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
@@ -10,17 +12,40 @@ export class ProductComponent implements OnInit {
 
   products: any = [];
 
+  productListResponse: ProductListResponse = {};
+  errorMessage: any = "";
+
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.displayProducts();
+    //this.displayProducts();
+    this.getProductList();
   }
 
 
   displayProducts(){
+    this.products = this.productService.getProducts();
+  }
 
-    this.products = this.productService.getProducts() ;
+  getProductList(){
+    this.errorMessage = '';
+    // Observer
+    var response = this.productService.getProductList()
+    .subscribe(httpResponse => {
 
+      this.productListResponse = httpResponse;
+
+      if(this.productListResponse.status === 'true'){
+        this.products = this.productListResponse.products;
+      }
+      else{
+        this.errorMessage = this.productListResponse.message;
+      }
+    },
+
+    (err: HttpErrorResponse) => {
+      console.log(err);
+    });
 
   }
 
